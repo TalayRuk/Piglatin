1bb3ca7d52b09e1b5a36e65d3b379af1fcfbbc94
Ian Wilcox and Vichitra Pool <ian+vichitra@>
2016-08-17 12:35:33 -0700

    added got result

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,21 @@
 // translating function
-
-
-
 var piglatin = function(piglatinphrase) {
-  alert()
+  for( var i=0 ; i<piglatinphrase.length; i++ ){
+    if(piglatinphrase.charAt(i) === 'a' ||
+      piglatinphrase.charAt(i) === 'e' ||
+      piglatinphrase.charAt(i) === 'i' ||
+      piglatinphrase.charAt(i) === 'o' ||
+      piglatinphrase.charAt(i) === 'u' ||
+      piglatinphrase.charAt(i) === 'y') {
+        alert("this is a vowel");
+        var result = piglatinphrase;
+      }else{
+        alert("not a vowel");
+        var result = piglatinphrase;
+      }
+  };
+  return result;
 };
-
-
-
-
-
-
-
 
 // page stuff and/or front end
 
@@ -19,5 +23,6 @@ $(function(){
   $(".btn").click(function(){
     var piglatinphrase = $("input#yourphrase").val();
     var result = piglatin(piglatinphrase);
+    $(".finalresult").text(result);
   });
 });
