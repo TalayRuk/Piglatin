c30974941587305fd0f24b3a86f6e7a44d87cb34
Ian Wilcox and Vichitra Pool <ian+vichitra@>
2016-08-17 10:44:28 -0700

    added js frontend

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// translating function
+
+
+
+var piglatin = function(piglatinphrase) {
+  alert()
+};
+
+
+
+
+
+
+
+
+// page stuff and/or front end
+
+$(function(){
+  $(".btn").click(function(){
+    var piglatinphrase = $("input#yourphrase").val();
+    var result = piglatin(piglatinphrase);
+  });
+});
